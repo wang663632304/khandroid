5858611749d5a3915219890daddfb93a70804d06
Ognyan Bankov <ogibankov@gmail.com>
2013-06-11 15:38:54 +0300

    changing return type

--- khandroid_lib/src/com/github/khandroid/misc/DbStorable.java
+++ khandroid_lib/src/com/github/khandroid/misc/DbStorable.java
@@ -4,5 +4,5 @@ import android.database.sqlite.SQLiteDatabase;
 
 public interface DbStorable {
 	long save(SQLiteDatabase dbc);
-	DbStorable loadByID(SQLiteDatabase dbc, long id);
+	boolean loadByID(SQLiteDatabase dbc, long id);
 }
