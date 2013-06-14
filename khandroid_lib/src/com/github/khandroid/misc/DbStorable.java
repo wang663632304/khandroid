4ae833c87a19a9d137e47b0bd8ab7fc624e997c3
Ognyan Bankov <ogibankov@gmail.com>
2013-06-14 13:01:19 +0300

    renaming method accourding to code conventions

--- khandroid_lib/src/com/github/khandroid/misc/DbStorable.java
+++ khandroid_lib/src/com/github/khandroid/misc/DbStorable.java
@@ -4,5 +4,5 @@ import android.database.sqlite.SQLiteDatabase;
 
 public interface DbStorable {
 	long save(SQLiteDatabase dbc);
-	boolean loadByID(SQLiteDatabase dbc, long id);
+	boolean loadById(SQLiteDatabase dbc, long id);
 }
